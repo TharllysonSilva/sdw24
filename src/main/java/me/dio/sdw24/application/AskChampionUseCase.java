22f97469c7e94addb91232d9a21386f812eb6749
Tarllysson Silva <tharllysonsilva@gmail.com>
2024-03-27 19:12:39 -0300

    aplicação com exceções

--- src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
+++ src/main/java/me/dio/sdw24/application/AskChampionUseCase.java
@@ -4,14 +4,16 @@ import me.dio.sdw24.domain.exception.ChampionNotFoundException;
 import me.dio.sdw24.domain.model.Champion;
 import me.dio.sdw24.domain.ports.ChampionsRepository;
 
-public record AskChampionsUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository) {
 
-    public String askChampion(Long championId, String question){
+    public String askChampion(Long championId, String question) {
 
         Champion champion = repository.findById(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
 
-        String championContext =  champion.generateContextByQuestion(question);
+        String championContext = champion.generateContextByQuestion(question);
+
+        // TODO: Evoluir a lógica de negócio para considerar a integração com IAs Generativas.
 
         return championContext;
     }
